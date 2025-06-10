4dfc5af0c9b74ed371b92d8ecf9e5b039bf76e6d
nairobi-j <jerinusrat001@gmail.com>
2025-06-11 01:58:51 +0600

    added frontend of loading posters

--- movies/src/main/java/com/example/movies/MovieController.java
+++ movies/src/main/java/com/example/movies/MovieController.java
@@ -4,10 +4,7 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 //api layer , concerns about task, getting req returns res, nothing else,
 // using service class to delegate the task of fetching from database and giving it to api layer,
 // calls allmovies, gets the list of movie returns with the status http.ok,
@@ -17,7 +14,7 @@ import java.util.Optional;
 //this is first official  restapi  controller
 @RestController
 @RequestMapping("/api/v1/movies")
-
+@CrossOrigin(origins = "http://localhost:3000")
 public class MovieController {
     @Autowired
     private MovieService movieService;
